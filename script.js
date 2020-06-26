b68275a75db98138d5f5a00fc37f0b42ac0f01c1
Melanie Gilman <melaniegilman259@yahoo.com>
2020-06-26 14:22:51 -0400

    questions and answers appear

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const startButton = document.getElementById('start-btn')
 const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+let shuffledQuestions, currentQuestionIndex
+
+
 startButton.addEventListener('click', startQuiz)
 
 
@@ -7,14 +12,46 @@ startButton.addEventListener('click', startQuiz)
 function startQuiz() {
     console.log('started')
     startButton.classList.add('hidden')
+    shuffledQuestions = questions.sort(() => Math.random() - .5)
+    currentQuestionIndex = 0
     questionContainerElement.classList.remove('hidden')
+    setNextQuestion()
+}
+
+
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex])
+}
+
+function showQuestion(question) {
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+        const button = document.createElement('button')
+        button.innerText = answer.text
+        button.classList.add('btn')
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        }
+        button.addEventListener('click', selectAnswer)
+        answerButtonsElement.appendChild(button)
+    })
+}
+
+
+
+function selectAnswer(e) {
 
 }
 
-function nextQuestion() {
-
-}
-
-function selectAnswer() {
-
-}
+const questions = [
+    {
+        question: 'A Boolean value will always be...',
+        answers:[
+            {text: 'True or False', correct: true},
+            {text: 'A number', correct: false},
+            {text: 'A string', correct: false},
+            {text: 'None of the above', correct: false}
+        ]
+    }
+];
