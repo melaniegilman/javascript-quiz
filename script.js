86055a6e292c7f21c358c2f722c931e828a09695
Melanie Gilman <melaniegilman259@yahoo.com>
2020-06-28 20:35:56 -0400

    final css and code cleanup

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ var selectAnswerTimeout = null
 let timeLeft = 10;
 let score = 0;
 let shuffledQuestions, currentQuestionIndex
-
+//when quiz restarts
 function resetState() {
     timeLeftDisplay.innerHTML = 10
     timeLeft = 10
@@ -22,8 +22,8 @@ function resetState() {
     score = 0
     initialInput.value = ""
     initialBtn.setAttribute("disabled", true)
-  }
-  //function to begin the quiz
+};
+//function to begin the quiz
 function startQuiz() {
     resetState()
     startButton.classList.add("hide")
@@ -33,7 +33,7 @@ function startQuiz() {
     currentQuestionIndex = 0
     questionContainerElement.classList.remove("hide")
     setNextQuestion();
-}
+};
 // timer variables
 var myTimer = null
 var timer = function(){
@@ -43,17 +43,15 @@ var timer = function(){
     }
     const time = --timeLeft
     timeLeftDisplay.innerHTML = time < 0 ? 0 : timeLeft
-    }
- //timer function
- function startTimer() {
+    };
+//timer function
+function startTimer() {
      myTimer = setInterval(timer, 1000)
-}
+};
 function stopTimer() {
    if (myTimer) {clearInterval(myTimer)}
    timeLeftDisplay.innerHTML = timeLeft < 0 ? 0 : timeLeft
-} 
-
-
+};
 //function to begin next question
 function setNextQuestion() {
     //change the visible answers
@@ -63,7 +61,7 @@ function setNextQuestion() {
     }
     showQuestion(shuffledQuestions[currentQuestionIndex])
 
-}
+};
 //function to make question appear
 function showQuestion(question) {
     questionElement.innerText = question.question
@@ -77,7 +75,7 @@ function showQuestion(question) {
         button.addEventListener('click', selectAnswer)
         answerButtonsElement.appendChild(button)
     })
-}
+};
 //shuffle through questions array
 function selectAnswer(e) {
     const selectedButton = e.target
@@ -95,13 +93,12 @@ function selectAnswer(e) {
             answerStatusEl.innerHTML = ""
         }
     }, 1000)
-}
-
+};
+//user enters initials
 function enterInitials() {
     initialEl.classList.remove("hide")
     questionContainerElement.classList.add("hide")
-}
-
+};
 //ensure initials are entered before saving
 function handleSubmitButtonState() {
     if (initialInput.value) {
@@ -109,27 +106,20 @@ function handleSubmitButtonState() {
     } else {
        initialBtn.setAttribute("disabled", true)
     }
- }
- //ensure that initials are entered before saving
- function handleSubmitButtonState() {
-    if (initialInput.value) {
-        initialBtn.removeAttribute("disabled")
-    } else {
-       initialBtn.setAttribute("disabled", true)
-    }
- }
+ };
+ //scores functions
  function displayScores() {
     initialEl.classList.add("hide")
     scoreDisplay.classList.remove("hide")
     startButton.innerText = "restart"
     startButton.classList.remove("hide")
- }
+ };
 function viewHighScores() {
     stopTimer()
     clearTimeout(selectAnswerTimeout)
     questionContainerElement.classList.add("hide")
     displayScores()
-}
+};
 function keepScore(isCorrect) {
     if(isCorrect) {
         score++
@@ -138,14 +128,13 @@ function keepScore(isCorrect) {
         timeLeft -=2
         console.log(timeLeft);
     }
-}  
-//save scores
+}; 
 function saveScore() {
     var scoreDiv = document.createElement("div")
     scoreDiv.innerHTML = "<h2>" + initialInput.value + "</h2><div>" + score + "</div"
     document.getElementById("scoreContainer").append(scoreDiv)
     displayScores()
-}
+};
 //array of questions and answers
 const questions = [
     {
@@ -194,7 +183,7 @@ const questions = [
         ]
     }
 ]
-
+//other logic
 document.addEventListener('DOMContentLoaded', () => {
     startButton.addEventListener('click', startQuiz)
     initialBtn.addEventListener("click", saveScore)
